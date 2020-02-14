fe58f914758d4ecc733e75efcb018eb105651be0
Alison Ludick <alison.ludick@gmail.com>
2020-02-14 11:24:25 +0200

    update media q again - last time

--- src/components/StarWarsCharacter.js
+++ src/components/StarWarsCharacter.js
@@ -4,19 +4,19 @@ const StarWarsCharacter = ({ data }) => {
 	return (
 		<div className="person">
 			<h2>{data.name}</h2>
-      {
-        data.birth_year !== 'n/a' && data.birth_year !== 'unknown' ?
-        <p><b>Birth Year:</b> {data.birth_year}</p> : null
-      }
+			{
+			data.birth_year !== 'n/a' && data.birth_year !== 'unknown' ?
+			<p><b>Birth Year:</b> {data.birth_year}</p> : null
+      		}
 			<p><b>Height:</b> {data.height}</p>
 			{
-				data.hair_color !== 'n/a' && data.hair_color !== 'none' ? 
-					<p><b>Hair Color:</b> {data.hair_color}</p> : null 
+			data.hair_color !== 'n/a' && data.hair_color !== 'none' ? 
+			<p><b>Hair Color:</b> {data.hair_color}</p> : null 
 			}
 			<p><b>Eye Color:</b> {data.eye_color}</p>
 			{
-				data.gender !== 'n/a' && data.gender !== 'none' ? 
-					<p><b>Gender:</b> {data.gender}</p> : null 
+			data.gender !== 'n/a' && data.gender !== 'none' ? 
+			<p><b>Gender:</b> {data.gender}</p> : null 
 			}
 		</div>
 	)
